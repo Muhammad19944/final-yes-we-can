46da7af85318b586cea1137b2e4467618bd94c8c
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2025-06-20 12:00:08 +0500

    test(composables): cover useExtendProps with unit tests

---
+++ composables/useExtendProps.test.ts
@@ -0,0 +1,65 @@
+import { it, expect, describe } from 'vitest'
+import { useExtendProps } from '~/composables/useExtendProps'
+import type { ColorType, SizeType } from '~/types/utils'
+
+describe('useExtendProps composable', () => {
+  it('composable should return library ui color for used components', () => {
+    const customColors = ['greyscale', 'white', 'gradient'] as const
+    type CustomColorsType = (typeof customColors)[number]
+
+    type UiColorsType = ColorType
+    type ExtendColorsType = ColorType | CustomColorsType
+
+    const { defineExtend } = useExtendProps<CustomColorsType, ExtendColorsType, UiColorsType>(customColors, ref('success'), 'primary')
+
+    expect(defineExtend.value).toEqual({
+      ui: 'success',
+      extend: null
+    })
+  })
+
+  it('composable should return extend custom color for used components', () => {
+    const customColors = ['greyscale', 'white', 'gradient'] as const
+    type CustomColorsType = (typeof customColors)[number]
+
+    type UiColorsType = ColorType
+    type ExtendColorsType = ColorType | CustomColorsType
+
+    const { defineExtend } = useExtendProps<CustomColorsType, ExtendColorsType, UiColorsType>(customColors, ref('gradient'), 'primary')
+
+    expect(defineExtend.value).toEqual({
+      ui: 'primary',
+      extend: 'gradient'
+    })
+  })
+
+  it('composable should return library ui size for used components', () => {
+    const customSizes = ['2xl', '3xl'] as const
+    type CustomSizesType = (typeof customSizes)[number]
+
+    type UiSizesType = Exclude<SizeType, '3xs' | '2xs' | CustomSizesType>
+    type ExtendSizesType = UiSizesType | CustomSizesType
+
+    const { defineExtend } = useExtendProps<CustomSizesType, ExtendSizesType, UiSizesType>(customSizes, ref('xl'), 'md')
+
+    expect(defineExtend.value).toEqual({
+      ui: 'xl',
+      extend: null
+    })
+  })
+
+  it('composable should return extend custom size for used components', () => {
+    const customSizes = ['2xl', '3xl'] as const
+    type CustomSizesType = (typeof customSizes)[number]
+
+    type UiSizesType = Exclude<SizeType, '3xs' | '2xs' | CustomSizesType>
+    type ExtendSizesType = UiSizesType | CustomSizesType
+
+    const { defineExtend } = useExtendProps<CustomSizesType, ExtendSizesType, UiSizesType>(customSizes, ref('2xl'), 'md')
+
+    expect(defineExtend.value).toEqual({
+      ui: 'md',
+      extend: '2xl'
+    })
+  })
+})
