b316bf950c5e8092dbbde9c979ff24d245e10335
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2025-07-02 10:30:32 +0500

    feat(CardJobSkills): add new component

---
+++ components/Card/Job/Skills.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import type { BaseBadgeEntity } from '~/components/Base/Badge/BaseBadge.vue'
+
+interface JobSkillsEntity {
+  card?: boolean
+  skills?: BaseBadgeEntity[]
+}
+</script>
+
+<script setup lang="ts">
+withDefaults(defineProps<JobSkillsEntity>(), {
+  card: true
+})
+</script>
+
+<template>
+  <BaseSheet
+    :card="card"
+    :ui="{
+      root: 'py-4 px-6'
+    }"
+  >
+    <BaseHeading
+      text="Talab etiladigan qobiliyatlar"
+      level="h7"
+      weight="semi"
+      color="text-(--color-greyscale-900)"
+      :ui="{
+        root: 'mb-3'
+      }"
+    />
+
+    <div class="flex flex-wrap gap-2">
+      <template
+        v-for="skill in skills"
+        :key="skill.label"
+      >
+        <BaseBadge
+          :label="skill.label"
+          color="greyscale"
+          size="lg"
+          :ui="{
+            base: 'text-(--color-greyscale-700)'
+          }"
+        />
+      </template>
+    </div>
+  </BaseSheet>
+</template>
--- pages/jobs.vue
+++ pages/jobs.vue
@@ -1,4 +1,22 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const items = ref([
+  {
+    label: 'Figma'
+  },
+  {
+    label: 'UX/UI dizayn'
+  },
+  {
+    label: 'Dashboard'
+  },
+  {
+    label: 'Website'
+  },
+  {
+    label: 'SEO'
+  }
+])
+</script>
 
 <template>
   <div class="p-20 space-y-5">
@@ -9,6 +27,8 @@
     <CardJobSize />
 
     <CardJobFiles />
+
+    <CardJobSkills :skills="items" />
   </div>
 </template>
 
